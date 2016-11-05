8eff24080a9765735270f9000dc05250fed8e9d9
Gerson Lobos <gerson.lobos@whitestratus.com>
2016-11-05 00:10:30 -0400

    adding database an junit test

---
+++ app/src/androidTest/java/com/lobos_google/weatherapp/FullTestSuite.java
@@ -0,0 +1,22 @@
+package com.lobos_google.weatherapp;
+
+/**
+ * Created by gersonlobos on 5/11/16.
+ */
+
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
